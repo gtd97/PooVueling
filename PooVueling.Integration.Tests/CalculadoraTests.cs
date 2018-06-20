018632dc5047e4df1ce1e03e5c67b75de7d05779
formacion <formacion@in2.es>
2018-06-20 15:41:56 +0200

    primer commit

---
+++ PooVueling.Integration.Tests/CalculadoraTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PooVueling;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooVueling.Tests
+{
+    [TestClass()]
+    public class CalculadoraTests
+    {
+        [TestMethod()]
+        public void CloneTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DivisionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void MultiplicacionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RestaTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SumaTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
